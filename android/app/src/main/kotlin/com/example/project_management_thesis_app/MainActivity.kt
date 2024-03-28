bb375ab85e841c808521b9e9de04fea21b129aab
Mobile Dev Digital Center <mobiledevdigitalcenter@gmail.com>
2024-03-28 09:54:52 +0700

    [Faris]: firebase init

--- android/app/src/main/kotlin/com/example/project_management_thesis_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project_management_thesis_app/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.project_management_thesis_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
