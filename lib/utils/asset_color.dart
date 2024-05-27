cbca73f8db420e2a7bbccc85ca6c20103c84bcd7
Faris Adilah Zakaria <faris.zakaria@binus.ac.id>
2024-05-27 10:39:46 +0700

    [Faris]: update utils

---
+++ lib/utils/asset_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AssetColor {
+  static const whitePrimary = Color.fromARGB(255, 251, 254, 249);
+  static const blackPrimary = Color.fromARGB(255, 25, 25, 35);
+  static const whiteBackground = Color.fromARGB(255, 241, 250, 238);
+  static const bluePrimaryAccent = Color.fromARGB(255, 168, 218, 220);
+  static const blueSecondaryAccent = Color.fromARGB(255, 69, 123, 157);
+  static const blueTertiaryAccent = Color.fromARGB(255, 0, 48, 73);
+  static const blueDisabled = Color.fromARGB(150, 168, 218, 220);
+}
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,5 +1,23 @@
-enum UserType { supervisor, admin, projectManager, staff }
+enum UserType {
+  supervisor,
+  admin,
+  projectManager,
+  staff,
+}
 
-enum CollectionType { users, projects, timelines, scheduleTasks }
+enum CollectionType {
+  users,
+  projects,
+  timelines,
+  scheduleTasks,
+  clients,
+  vendors,
+  payments,
+}
+
+enum ExternalType {
+  vendor,
+  client,
+}
 
 class Constant {}
