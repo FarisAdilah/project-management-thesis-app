c4393938c00b7bbedb2f8730e1d340997c351237
Faris Adilah Zakaria <faris.zakaria@binus.ac.id>
2024-06-06 00:32:21 +0700

    [Faris]: add user delete popup

--- lib/pages/staffPage/staffList/controller_staff_list.dart
+++ lib/pages/staffPage/staffList/controller_staff_list.dart
@@ -3,9 +3,11 @@ import 'dart:convert';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:project_management_thesis_app/globalComponent/textCustom/custom_text.dart';
 import 'package:project_management_thesis_app/pages/staffPage/staffForm/staff_form.dart';
 import 'package:project_management_thesis_app/repository/user/dataModel/user_dm.dart';
 import 'package:project_management_thesis_app/repository/user/user_repository.dart';
+import 'package:project_management_thesis_app/utils/asset_color.dart';
 import 'package:project_management_thesis_app/utils/constant.dart';
 import 'package:project_management_thesis_app/utils/helpers.dart';
 
@@ -66,7 +68,76 @@ class StaffListController extends GetxController {
     _getUsersData();
   }
 
-  onDeleteUser(UserDM user) async {
+  onDeleteUser(UserDM user) {
+    Get.dialog(
+      AlertDialog(
+        title: const CustomText(
+          "Delete User",
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: AssetColor.greyBackground,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 16,
+        ),
+        content: CustomText(
+          "Are you sure you want to delete ${user.name}?",
+          fontSize: 18,
+        ),
+        actionsAlignment: MainAxisAlignment.center,
+        actionsPadding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 16,
+        ),
+        actions: [
+          TextButton(
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(
+                AssetColor.orangeButton,
+              ),
+              padding: WidgetStatePropertyAll(
+                EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 16,
+                ),
+              ),
+            ),
+            onPressed: () {
+              Get.back();
+            },
+            child: const CustomText(
+              "Cancel",
+              color: AssetColor.whiteBackground,
+            ),
+          ),
+          TextButton(
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(
+                AssetColor.redButton,
+              ),
+              padding: WidgetStatePropertyAll(
+                EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 16,
+                ),
+              ),
+            ),
+            onPressed: () {
+              Get.back();
+              _deleteUser(user);
+            },
+            child: const CustomText(
+              "Delete",
+              color: AssetColor.whiteBackground,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _deleteUser(UserDM user) async {
     isLoading.value = true;
     bool isDeleted = await _userRepository.deleteUser(user);
     if (isDeleted) {
